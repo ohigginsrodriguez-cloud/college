8b52b6cfec0f28d549709be16a9c9eab9583ce54
O'Higgins Rodriguez Hernandez <ohigginsrodriguez@gmail.com>
2026-08-29 22:11:35 -0600

    Using .Resource and request

--- simulation/customer.py
+++ simulation/customer.py
@@ -1,17 +1,30 @@
 import simpy
 
 
-def customer(env):
+def customer(env, id_customer, arrival_time, cashier):
 
-    print(f"Customer arrives at t={env.now}")
+    yield env.timeout(arrival_time)
 
-    yield env.timeout(5)
+    print(f"Customer {id_customer} arrives at t={env.now}")
 
-    print(f"Customer leaves at t={env.now}")
+    with cashier.request() as request:
+
+        yield request
+
+        print(f"The client {id_customer} is attended at t={env.now}")
+
+        yield env.timeout(2)
+
+        print(f"Customer {id_customer} leaves at t={env.now}\n")
 
 
 env = simpy.Environment()
 
-env.process(customer(env))
+# Resourse can only be used by 1 process at time
+cashier = simpy.Resource(env, capacity=1)
+
+env.process(customer(env, 1, 0, cashier))
+env.process(customer(env, 2, 1.5, cashier))
+env.process(customer(env, 3, 4, cashier))
 
 env.run()
