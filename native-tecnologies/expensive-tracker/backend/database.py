abb1a5f69a987e6c10c420c03de0b6a9375a535f
O'Higgins Rodriguez Hernandez <ohigginsrodriguez@gmail.com>
2026-09-13 02:06:26 -0600

    feat: database conection

---
+++ native-tecnologies/expensive-tracker/backend/database.py
@@ -0,0 +1,41 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()  # read variables from a .env file and sets them in os.environ
+
+try:
+    conn = psycopg2.connect(
+        f"dbname={os.getenv('DB_NAME')} "
+        f"user={os.getenv('DB_USER')} "
+        f"host={os.getenv('DB_HOST')} "
+        f"password={os.getenv('DB_PASSWORD')} "
+    )
+except:
+    print("I am unable to connect to the database")
+
+# we use a context manager to scope the cursor session
+with conn.cursor() as curs:
+
+    try:
+        # simple single row system query
+        curs.execute("SELECT version()")
+
+        # returns a single row as a tuple
+        single_row = curs.fetchone()
+
+        # use an f-string to print the single tuple returned
+        print(f"{single_row}")
+
+        # simple multi row system query
+        curs.execute("SELECT query, backend_type FROM pg_stat_activity")
+
+        # a default install should include this query and some backend workers
+        many_rows = curs.fetchmany(5)
+
+        # use the * unpack operator to print many_rows which is a Python list
+        print(*many_rows, sep="\n")
+
+    # a more robust way of handling errors
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
